4d875a7b291ca7e99e0ddee71d4527ba6627cb14
mohanr57 <109952469+mohanr57@users.noreply.github.com>
2022-09-12 15:21:40 +0530

    commite3

--- Main.py
+++ Main.py
@@ -1,44 +1,68 @@
 class Node:
+  """
+  Class implementing node of the LinkedList
+  Attributes:
+    -> data - data held by the node
+    -> next - link to the next node
+  """
   def __init__(self, data):
     self.data = data
     self.next = None
 
 
-class Stack:
+class Queue:
+  """
+  Class implementing Queue as a LinkedList
+  """
   def __init__(self):
+    """
+    Initialises Queue object with pointers head and tail set to None
+    """
     self.head = None
+    self.tail = None
 
-  def push(self, data) -> None:
+  def enqueue(self, data) -> None:
+    """
+    Adds node containing data passed to the rear of the queue
+    """
     new = Node(data)
-    new.next = self.head
-    self.head = new
-    
+    if not self.tail is None:
+      self.tail.next = new
+    if self.head is None:
+      self.head = new
+    self.tail = new
 
-  def pop(self) -> None:
+  def dequeue(self) -> None:
+    """
+    Removes node from the rear of the queue
+    """
     if not self.head is None:
-        self.head = self.head.next
+      self.head = self.head.next
+      if self.head is None:
+        self.tail = None
 
-  def status(self):
+  def status(self) -> None:
     """
-    It prints all the elements of stack.
+    It prints all the elements of Queue.
     """
-    current = self.head
-    while current is not None:
-        print(current.data, end = "=>")
-        current = current.next
-    print("None")
+    elements = ""
+    curr = self.head
+    while not curr is None:
+      elements += str(curr.data) + "=>"
+      curr = curr.next
+    print(elements + "None")
 
 
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status()
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
